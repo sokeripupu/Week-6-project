64dd1ca38b13fa48b7ac3cf4ead9448204aade95
Rachel Courtney <rcourtney@clsphila.org>
2022-11-10 21:18:43 -0500

    Added another test and new game result

--- war.test.js
+++ war.test.js
@@ -1,13 +1,20 @@
 const expect = chai.expect;
 
-player1 = [];
-player2 = [];
 
-describe("Test Deal function", function () {
-  describe;
+describe("Deck related functions", function () {
+  it("A new deck should contain 52 cards", function(){
+    let deckTest = new Deck;
 
-  it("Both arrays should contain 26 elements", function () {
-    deal(deck, player1, player2);
+    expect(deckTest.cardsInDeck.length).to.equal(52);
+  })
+  
+  it("Both player arrays should contain 26 cards after dealing", function () {
+    let dealTest = new Deck;  
+    player1 = [];
+      player2 = [];
+    deal(dealTest, player1, player2);
+  
+
     expect(player1.length).to.equal(26) && expect(player2.length).to.equal(26);
   });
 });
